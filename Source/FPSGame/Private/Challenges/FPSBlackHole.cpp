6d3cf738cac7f8b26c74fa9903d294b75b5f6069
hongilywang <hongilywang@sina.com>
2021-04-29 18:30:51 +0800

    黑洞Challenges挑战！

---
+++ Source/FPSGame/Private/Challenges/FPSBlackHole.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Challenges/FPSBlackHole.h"
+
+// Sets default values
+AFPSBlackHole::AFPSBlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	InnerSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
+	OuterSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
+
+	RootComponent = MeshComponent;
+	InnerSphereComponent->SetupAttachment(MeshComponent);
+	OuterSphereComponent->SetupAttachment(MeshComponent);
+	
+	MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	InnerSphereComponent->SetSphereRadius(100);
+	InnerSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSBlackHole::OverlapInnerSphere);
+	
+	OuterSphereComponent->SetSphereRadius(3000);
+	
+}
+
+void AFPSBlackHole::OverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		OtherActor->Destroy();
+	}
+}
+
+// Called when the game starts or when spawned
+void AFPSBlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSBlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TArray<UPrimitiveComponent*> OverlappingComps;
+	OuterSphereComponent->GetOverlappingComponents(OverlappingComps);
+
+	const float SphereRadius = OuterSphereComponent->GetScaledSphereRadius();
+	const float ForceStrength = -2000;
+	for (int i = 0; i < OverlappingComps.Num(); ++i)
+	{
+		UPrimitiveComponent* PrimComp = OverlappingComps[i];
+		PrimComp->AddRadialForce(GetActorLocation(), SphereRadius, ForceStrength, ERadialImpulseFalloff::RIF_Constant, true);
+	}
+}
+
