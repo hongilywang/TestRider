b35bb88312e9fc6d7269e94fd8c6200923132989
hongilywang <hongilywang@sina.com>
2021-05-10 20:50:11 +0800

    添加Overlap组件

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -1,6 +1,20 @@
 #include "FPSExtractionZone.h"
 
+#include "FPSCharacter.h"
+
 AFPSExtractionZone::AFPSExtractionZone()
 {
+	OverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComponent"));
+	RootComponent = OverlapComponent;
+	OverlapComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	OverlapComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComponent->SetBoxExtent(FVector(200));
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+	OverlapComponent->SetHiddenInGame(false);
+}
 
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlap!"));
 }
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "Components/BoxComponent.h"
 #include "GameFramework/Actor.h"
 #include "FPSExtractionZone.generated.h"
 
@@ -11,4 +13,11 @@ class FPSGAME_API AFPSExtractionZone : public AActor
 
 public:
 	AFPSExtractionZone();	
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* OverlapComponent;
+
+	UFUNCTION()
+	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 };
